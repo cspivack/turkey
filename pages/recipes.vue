d9cfa6e54fff826fccb8a5fccc29bc92bc981135
cspivack <spivack.craig@gmail.com>
2020-12-05 16:26:18 -0500

    Initial commit

---
+++ pages/recipes.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Recipes
+      </h1>
+      <div>
+        <a v-for="recipe in recipes" :key="recipe.name" :href="recipe.link" target="_blank">
+          {{ recipe.name }}
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      recipes: [
+        {
+          name: 'Cranberry sauce',
+          link: 'https://cooking.nytimes.com/recipes/10267-cranberries-and-port-wine'
+        },
+        {
+          name: 'Turkey',
+          link: 'https://recipes.craigspivack.com/posts/spatchcocked-turkey-with-herb-butter'
+        },
+        {
+          name: 'Collard greens',
+          link: 'https://recipes.craigspivack.com/posts/vegan-collard-greens'
+        },
+        {
+          name: 'Stuffing',
+          link: 'https://www.thecomfortofcooking.com/2010/02/old-fashioned-bread-stuffing.html'
+        },
+        {
+          name: 'Mac and cheese',
+          link: 'https://recipes.craigspivack.com/posts/simple-mac-cheese'
+        },
+        {
+          name: 'Gravy',
+          link: 'https://recipes.craigspivack.com/posts/turkey-gravy'
+        }
+      ]
+    }
+  }
+}
+</script>
